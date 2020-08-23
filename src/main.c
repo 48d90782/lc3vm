8c5a6639bdaf13a8f5283f5f698a9d551dbc0239
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-08-23 23:40:02 +0300

    start main

--- src/main.c
+++ src/main.c
@@ -1,4 +1,18 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <signal.h>
+/* unix */
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
+
+u_int16_t memory[UINT16_MAX];
 
 int main() {
     return EXIT_SUCCESS;
