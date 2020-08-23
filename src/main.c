70a5acd0519546d150d72d9050989d4e2a1f4c6b
Valery Piashchynski <piashchynski_valery@hotmail.com>
2020-08-23 23:49:59 +0300

    Iml registers enum

--- src/main.c
+++ src/main.c
@@ -14,6 +14,20 @@
 
 u_int16_t memory[UINT16_MAX];
 
+enum {
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC, /*Program counter*/
+    R_COND,
+    R_COUNT,
+};
+
 int main() {
     return EXIT_SUCCESS;
 }
